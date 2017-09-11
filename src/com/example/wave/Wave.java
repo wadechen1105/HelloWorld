0bd9e72d32b429974594d101a19c0681a2f1ea6d
wadechen1105 <wadechen1105@gmail.com>
2017-09-11 14:39:15 +0800

    Update Wave.java

--- src/com/example/wave/Wave.java
+++ src/com/example/wave/Wave.java
@@ -1,19 +1,3 @@
-/*****************************************************************************
- * Copyright (C) 2011 Acer, Inc. All Rights Reserved.
- *
- * This program is an unpublished copyrighted work which is proprietary
- * to Acer, Inc. and contains confidential information that is not to
- * be reproduced or disclosed to any other person or entity without
- * prior written consent from Acer, Inc. in each and every instance.
- *
- * WARNING:  Unauthorized reproduction of this program as well as
- * unauthorized preparation of derivative works based upon the
- * program or distribution of copies by sale, rental, lease or
- * lending are violations of federal copyright laws and state trade
- * secret laws, punishable by civil and criminal penalties.
- *
- *****************************************************************************/
-
 package com.example.wave;
 
 import android.content.Context;
